da1c1b1c0e0b5d56eaf4ad782c50752b2691d3b2
Therdsak <Therdsak.p@augmentis.biz>
2016-07-15 11:20:22 +0700

    Commit1

--- app/src/main/java/com/augmentis/ayp/aypquiz/QuizActivity.java
+++ app/src/main/java/com/augmentis/ayp/aypquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.augmentis.ayp.aypquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,19 +26,70 @@ public class QuizActivity extends AppCompatActivity {
 
     int currentIndex;
 
+    private static  final String TAG = "AYPQUIZ";
+    private static  final String Index = "Index";
+    public QuizActivity() {
+        super();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"On pause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"On resume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"On strat");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"On stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"On destroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG,"State is saving");
+        outState.putInt(Index,currentIndex);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"On create");
         setContentView(R.layout.activity_quiz);
 
         trueButton = (Button) findViewById(R.id.true_button);
         falseButton = (Button) findViewById(R.id.false_button);
         nextButton = (Button) findViewById(R.id.next_button);
         previousButton = (Button) findViewById(R.id.previous_button);
+
         questionText = (TextView) findViewById(R.id.text_question);
 
+        if(savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt(Index, 0);
+        }else{
+            currentIndex = 0 ;
+        }
 
-        currentIndex = 0;
+
         updateQuestion();
 
         trueButton.setOnClickListener(new View.OnClickListener() {
